16e7f8fd43c9f02eaf86324f7778f5f3da27e811
Bignone Camila <camilabignone@gmail.com>
2016-02-07 15:22:21 -0300

    agrega datos del cliente

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,4 +9,23 @@ class Client < ActiveRecord::Base
 		now = Time.now.utc.to_date
 		now.year - self.birthdate.year - (birthdate.to_date.change(:year => now.year) > now ? 1 : 0)
 	end
+
+	def billing_per_year
+		hsh = {}
+   		self.bills.each{|b| hsh[b.date.year] = hsh.fetch(b.date.year,0) + b.amount}
+   		hsh.sort
+	end
+
+	def number_per_month
+		hsh = {}
+		bills_in_year = self.bills.select{|b| b.date.year == Date.today.year }
+		bills_in_year.each{|b| hsh[b.date.month] = hsh.fetch(b.date.month,0) + 1}
+		hsh.sort
+	end
+
+	def more_invoiced
+		hsh = {}
+		self.bills.each{|b| hsh[b.person.cuit] = hsh.fetch(b.person.cuit,0) + 1}
+		hsh.sort_by {|cuil, cant| cant }.first(5)
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+clients = Client.create([
+	{name: "camila", surname: "bignone", birthdate: Date.new(1994,1,11), genre: "f", dni: 31771994, cuil:27377719946},
+	{name: "sarasa" , surname: "sarasa", birthdate:Date.new(1980,10,1) ,genre: "m", dni: 300000000 , cuil:30300000001 },
+	{name: "juan" , surname: "perez" , birthdate:Date.new(1956,12,12) ,genre:"m", dni: 12238256 , cuil: 13122582684 },
+	{name: "quien", surname: "gomez" , birthdate:Date.new(1999,10,2) ,genre:"m", dni:9999999 , cuil:9999999999 }
+	])
+
+peoples = Person.create([
+	{name: "soy una persona", cuit: 12345678999},
+	{name: "persona dos", cuit: 22334455667}
+	])
+	
+bills = Bill.create([{ description: "dos", amount: 100, date: Date.today, client_id:2, person_id:1 }])
